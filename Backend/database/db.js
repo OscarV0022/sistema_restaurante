3f95d800bb677df4ebf033b318b963c330554a72
OscarV0022 <mundococ0012@gmail.com>
2026-06-04 17:09:01 -0600

    pruebas

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,10 +1,22 @@
+/*const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'oscar',      
+  password: '13112016',      
+  database: 'restaurante_provisional',
+  connectionLimit: 5
+});
+
+module.exports = pool;*/
+
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: process.env.DB_HOST || 'db',      
-  user: process.env.DB_USER || 'root',    
-  password: process.env.DB_PASS || 'password_seguro', 
-  database: process.env.DB_NAME || 'restaurante',     
+  host: process.env.DB_HOST || 'db',      // Ahora busca el servicio llamado 'db'
+  user: process.env.DB_USER || 'root',    // Usa root (según tu docker-compose)
+  password: process.env.DB_PASS || 'password_seguro', // Usa tu clave real
+  database: process.env.DB_NAME || 'restaurante',     // Usa la BD 'restaurante'
   connectionLimit: 5
 });
 
