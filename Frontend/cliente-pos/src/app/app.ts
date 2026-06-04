374c538946040a092629fdbc61e7ab2c09ea8bb6
OscarV0022 <mundococ0012@gmail.com>
2026-06-03 21:51:19 -0600

    frontend

--- Frontend/cliente-pos/src/app/app.ts
+++ Frontend/cliente-pos/src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd} from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'Chiantlequita';
+  showHeader = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showHeader = event.url !== '/login';
+      }
+    });
+  }
 }
--- Frontend/cliente-pos/src/app/components/header/header.ts
+++ Frontend/cliente-pos/src/app/components/header/header.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrls: ['./header.css']
 })
 export class Header {
+  constructor(private router: Router) {}
 
+  getRole() { return localStorage.getItem('rol'); }
+
+  canAccess(roles: string[]): boolean {
+    const role = this.getRole();
+    if (!role) return false;
+    if (role === 'admin') return true;
+    return roles.includes(role);
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
